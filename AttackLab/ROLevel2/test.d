5a07731e68b872b987131836dda315d09010c1c2
zhuwell <839161674@qq.com>
2023-06-16 01:24:52 +0800

    Return-Oriented Programming Level2 is solved

---
+++ AttackLab/ROLevel2/test.d
@@ -0,0 +1,10 @@
+
+test.o：     文件格式 elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	b8 fb 78 90 90       	mov    $0x909078fb,%eax
+   5:	8d 87 48 89 c7 c3    	lea    -0x3c3876b8(%rdi),%eax
+   b:	8d 87 51 73 58 90    	lea    -0x6fa78caf(%rdi),%eax
---
+++ AttackLab/ROLevel2/test.s
@@ -0,0 +1,3 @@
+mov $0x909078fb,%eax
+lea -0x3c3876b8(%rdi),%eax
+lea -0x6fa78caf(%rdi),%eax
