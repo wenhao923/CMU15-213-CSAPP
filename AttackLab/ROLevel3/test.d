e017f945db9cb93f6b51b1511ba6e4c670446a97
zhuwell <839161674@qq.com>
2023-06-19 02:05:53 +0800

    暂存

---
+++ AttackLab/ROLevel3/test.d
@@ -0,0 +1,25 @@
+
+./test.o：     文件格式 elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	8d 87 48 89 c7 c3    	lea    -0x3c3876b8(%rdi),%eax
+   6:	8d 87 51 73 58 90    	lea    -0x6fa78caf(%rdi),%eax
+   c:	48 8d 04 37          	lea    (%rdi,%rsi,1),%rax
+  10:	8d 87 89 ce 78 c9    	lea    -0x36873177(%rdi),%eax
+  16:	8d 87 8d d1 20 db    	lea    -0x24df2e73(%rdi),%eax
+  1c:	8d 87 41 48 89 e0    	lea    -0x1f76b7bf(%rdi),%eax
+  22:	8d 87 89 ce 90 90    	lea    -0x6f6f3177(%rdi),%eax
+  28:	8d 87 89 c2 00 c9    	lea    -0x36ff3d77(%rdi),%eax
+  2e:	8d 87 89 ce 38 c0    	lea    -0x3fc73177(%rdi),%eax
+  34:	8d 87 c8 89 e0 c3    	lea    -0x3c1f7638(%rdi),%eax
+  3a:	8d 87 89 c2 84 c0    	lea    -0x3f7b3d77(%rdi),%eax
+  40:	8d 87 48 89 e0 c7    	lea    -0x381f76b8(%rdi),%eax
+  46:	8d 87 89 ce 92 c3    	lea    -0x3c6d3177(%rdi),%eax
+  4c:	8d 87 08 89 e0 90    	lea    -0x6f1f76f8(%rdi),%eax
+  52:	8d 87 89 c2 c7 3c    	lea    0x3cc7c289(%rdi),%eax
+  58:	8d 87 89 c2 60 d2    	lea    -0x2d9f3d77(%rdi),%eax
+  5e:	48 98                	cltq   
+  60:	48 8d 04 07          	lea    (%rdi,%rax,1),%rax
---
+++ AttackLab/ROLevel3/test.s
@@ -0,0 +1,18 @@
+lea -0x3c3876b8(%rdi),%eax
+lea -0x6fa78caf(%rdi),%eax
+lea (%rdi,%rsi,1),%rax
+lea -0x36873177(%rdi),%eax
+lea -0x24df2e73(%rdi),%eax
+lea -0x1f76b7bf(%rdi),%eax
+lea -0x6f6f3177(%rdi),%eax
+lea -0x36ff3d77(%rdi),%eax
+lea -0x3fc73177(%rdi),%eax
+lea -0x3c1f7638(%rdi),%eax
+lea -0x3f7b3d77(%rdi),%eax
+lea -0x381f76b8(%rdi),%eax
+lea -0x3c6d3177(%rdi),%eax
+lea -0x6f1f76f8(%rdi),%eax
+lea 0x3cc7c289(%rdi),%eax
+lea -0x2d9f3d77(%rdi),%eax
+cltq
+lea (%rdi,%rax,1),%rax
