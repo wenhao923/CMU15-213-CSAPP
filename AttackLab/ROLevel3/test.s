6dbf7b64fb34f7c18c3413203b0bc3883ebf2906
zhuwenhao <zhuwell839@gmail.com>
2023-06-19 20:46:50 +0800

    Update test.s

--- AttackLab/ROLevel3/test.s
+++ AttackLab/ROLevel3/test.s
@@ -1,18 +1,43 @@
+movl   $0x909078fb, %eax
 lea -0x3c3876b8(%rdi),%eax
 lea -0x6fa78caf(%rdi),%eax
+movl   $0xc7c78948, (%rdi)
+movl   $0x9258c254, (%rdi)
+movl   $0xc78d4863, (%rdi)
+movl   $0x90c78948, (%rdi)
+movl   $0xc3905829, %eax
 lea (%rdi,%rsi,1),%rax
+movl   $0x90c2895c, %eax
+movl   $0x9090d199, (%rdi)
 lea -0x36873177(%rdi),%eax
 lea -0x24df2e73(%rdi),%eax
+movl   $0xc048d189, %eax
+movl   $0xc084d181, (%rdi)
 lea -0x1f76b7bf(%rdi),%eax
+movl   $0xc908c288, (%rdi)
 lea -0x6f6f3177(%rdi),%eax
+movl   $0xc1e08948, %eax
 lea -0x36ff3d77(%rdi),%eax
 lea -0x3fc73177(%rdi),%eax
+movl   $0xdb08ce81, (%rdi)
+movl   $0xc938d189, %eax
 lea -0x3c1f7638(%rdi),%eax
 lea -0x3f7b3d77(%rdi),%eax
 lea -0x381f76b8(%rdi),%eax
+movl   $0xd208d199, %eax
+movl   $0xc9c4c289, %eax
+movl   $0x91e08948, (%rdi)
 lea -0x3c6d3177(%rdi),%eax
+movl   $0xdb08d189, %eax
+movl   $0xc391d189, (%rdi)
+movl   $0xd238c281, (%rdi)
+movl   $0xc908ce09, (%rdi)
 lea -0x6f1f76f8(%rdi),%eax
 lea 0x3cc7c289(%rdi),%eax
+movl   $0xc020ce88, %eax
+movl   $0xc2e08948, (%rdi)
 lea -0x2d9f3d77(%rdi),%eax
+movl   $0xd220ce8d, %eax
+movl   $0x90e08948, (%rdi)
 cltq
 lea (%rdi,%rax,1),%rax
